0af9d0b42b8da613a913a2b182ebc27ad115e82f
Mahipals06 <28045771+Mahipals06@users.noreply.github.com>
2023-06-17 14:33:52 +0530

    Create decimal_to_binary.java

---
+++ decimal_to_binary.java
@@ -0,0 +1,26 @@
+//decimal to binary using stack
+import java.util.*;
+public class Decimal_to_Binary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter Number- ");
+		int digit=sc.nextInt();
+		int r=0;//declaring remainder and quotient
+		int binary=0;
+		Stack<Integer> s=new Stack<Integer>();
+		while(digit>0) {
+		
+			r=digit%2;
+			digit=digit/2;
+			s.push(r);
+		}
+		while(!s.isEmpty()) {
+			binary=binary*10+s.pop();
+		}
+		System.out.print("binary number is " +binary);
+		sc.close();
+	}
+
+}
