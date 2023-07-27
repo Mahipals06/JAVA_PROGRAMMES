1caa0667a363fc9036788facc1354b38c974b8e9
Mahipals06 <28045771+Mahipals06@users.noreply.github.com>
2023-07-27 10:36:35 +0530

    Create LeetCode#230.java

---
+++ LeetCode#230.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+          List<Integer> re=new ArrayList();
+          iot(root,re);
+          return re.get(k-1);
+    }
+     public void iot(TreeNode root,List<Integer> res){
+        if(root==null)
+        return;
+
+        iot(root.left,res);
+        res.add(root.val);
+        iot(root.right,res); 
+    }
+}
