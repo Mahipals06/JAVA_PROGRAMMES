8673eae892db258babd9a5849ef62a6053b75598
Mahipals06 <28045771+Mahipals06@users.noreply.github.com>
2023-07-31 21:22:30 +0530

    Create AdjacencyList.java

---
+++ AdjacencyList.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Classroom{
+  static class Edge{
+    int src;
+    int des;
+    public Edge(int src,int des){
+      this.src=src;
+      this.des=des;
+    }
+  }
+  public static void main(String args[]){
+    int V=4;//graph containing 4 vertices
+    ArrayList<Edge> graph[]=new ArrayList[V];    
+
+  }
+}
